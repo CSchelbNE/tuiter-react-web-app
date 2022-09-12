da562ae5f1e494ba526de317d086db234edda00b
Cschelb <85119920+Cschelb@users.noreply.github.com>
2022-09-12 18:17:01 -0400

    changed banner

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CS5610 Week 1
         </p>
         <a
           className="App-link"
