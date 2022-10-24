94e106e267d0a52683483683491deb8cd935e4e7
Cschelb <85119920+Cschelb@users.noreply.github.com>
2022-10-23 22:16:23 -0400

    experiment after readding the manifest and the index.html

---
+++ src/Cxz.js
@@ -0,0 +1,13 @@
+const Ewq = ({asd, sdf}) => {
+    const cxz = asd + sdf;
+
+    return(
+        <>
+            Zxc = {asd}<br/>
+            Xcv = {sdf}<br/>
+            Cvb = {asd + sdf}<br/>
+            <h1>{cxz}</h1>
+        </>
+    )
+}
+export default Ewq;
---
+++ src/labs/a6/hellwolrld.js
@@ -0,0 +1,9 @@
+import HelloWorld from "./hello-world"
+const App = () => {
+    return(
+        <>
+            <HelloWorld asd={"23"} sdf={"34"}/>
+        </>
+    )
+}
+export default App;
